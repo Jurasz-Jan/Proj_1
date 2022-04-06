33454917a56fa0cca097f46aa9c30b61a9ec99a0
Jan Jurasz <jan.jurasz@student.uj.edu.pl>
2022-04-06 14:58:37 +0200

    e
    ee

--- Database.cpp
+++ Database.cpp
@@ -28,23 +28,50 @@ class Carmander : public Pekomon
 public:
 	std::string classy;
 	
-    Carmander(){
-        classy="Car";
-        
-    }
+    Carmander(int element){
+        switch(element){
+            case 0:
+                type="Dinosaur";
+            break;
+            case 1:
+                type="Car";
+            break;
+            case 2:
+                type="HobByte";
+            break;
+            case 3:
+                type="Coconut";
+            break;
+            case 4:
+                type="Penguin from Madagascar";
+            break;
+            case 5:
+                type="Demon";
+            break;
+            case 6:
+                type="Video Game character";
+            break;
+            case 7:
+                type="Door";
+            break;
+            default:
+                type="normal";
+    // code block
+        }
+            }
 };
 
 // main function
 int main()
 {
-	Carmander obj1;
+	Carmander Suzuki(1);
 		
 	// An object of class child has all data members
 	// and member functions of class parent
 	
 	
-	std::cout << "Child id is: " << obj1.classy << '\n';
-	std::cout << "Parent id is: " << obj1.type << '\n';
+	std::cout << "Child id is: " << Suzuki.classy << '\n';
+	std::cout << "Parent id is: " << Suzuki.type << '\n';
 		
 	return 0;
 }
